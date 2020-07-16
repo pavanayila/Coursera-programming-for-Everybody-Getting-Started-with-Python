399cbac335ca22ae70747bd692209de79bac0d0c
pavanayila <67941868+pavanayila@users.noreply.github.com>
2020-07-16 20:50:35 +0530

    Add files via upload

---
+++ Week 3 Assignment-HelloWorld.py
@@ -0,0 +1,15 @@
+#Write a program that uses a print statement to say 'hello world' as shown in 'Desired Output'.
+
+
+# the code below almost works
+print("hello world")
+
+
+#Your Output:
+#-----------
+#hello world
+
+
+#Desired Output:
+#--------------
+#hello world
